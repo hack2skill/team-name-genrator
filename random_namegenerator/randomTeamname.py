3d92def99e0e3598112707235a5dfdcd18303fd8
sagar <sagarzendex@gmail.com>
2022-10-30 17:35:45 +0530

    randomTeamgeneeratorusinPython

---
+++ random_namegenerator/randomTeamname.py
@@ -0,0 +1,4 @@
+import random
+array1 =[ 'Pro', 'Alpha', 'Diva', 'Super', 'True']
+array2 =['Techies', 'Coders', 'Champs', 'Hackers', 'Phantoms']
+print(array1[random.choice([0,1,2,3,4])]+array2[random.choice([0,1,2,3,4])])
